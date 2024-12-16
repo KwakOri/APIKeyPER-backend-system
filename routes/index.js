f59d485b67d7946d059decb865b185fe6d897c92
kwakjaehun <jh5414092@icloud.com>
2024-12-16 18:16:36 +0900

    Feat: add pg module - index router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
+const client = require("../config/db");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send(JSON.stringify({ message: "CI/CD TEST" }));
+
+router.get("/", async (req, res, next) => {
+  const query = "SELECT * FROM users";
+  const { rows } = await client.query(query);
+  res.send(JSON.stringify({ data: rows }));
 });
 
 module.exports = router;
