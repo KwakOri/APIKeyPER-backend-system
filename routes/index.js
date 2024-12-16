594ae4eda46b92cbf5d5a1914af20eaa4ad547c8
kwakjaehun <jh5414092@icloud.com>
2024-12-16 15:21:57 +0900

    Feat: changee index route file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send(JSON.stringify({ message: "Home Page" }));
 });
 
 module.exports = router;
